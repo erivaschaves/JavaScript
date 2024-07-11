b343876d494595f7f9a21ee50e34b6c809d197a0
ErivaldoChaves <erivas_12@hotmail.com>
2024-07-11 12:15:58 -0300

    ex016

--- Aula013/ambiente.js
+++ Aula013/ambiente.js
@@ -1,6 +1,13 @@
 //console.log('Tudo bem?')
 var c = 1
-while(c<=6){
+do{
     console.log('Tudo bem?')
  c++
-}
+
+}while(c<=6)
+
+/*var c = 1
+while(c<=3){
+    console.log('Tudo bem?')
+ c++
+}*/
---
+++ Aula14ex/ex016/script.js
@@ -0,0 +1,27 @@
+/*function contar(){
+    var inicio = window.document.getElementById('txtini')
+    var fim = window.document.querySelector('div#txtfim')
+    fim = Number(fim.value)
+    var passo = window.document.getElementById('txtpasso')
+    passo = Number(passo.value)
+    var res = Number(inicio.value)
+    inicio = 5
+    fim = 12
+    passo = 2
+    res = inicio
+    while(res<fim){
+    console.log(res)
+    res = res + passo
+}
+    
+    
+}*/ 
+
+inicio = 5
+fim = 12
+passo = 2
+res = inicio
+while(res<fim){
+    console.log(res)
+    res = res + passo
+}
