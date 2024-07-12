187098b561ea29f23330e6e2cf862bba16235201
ErivaldoChaves <erivas_12@hotmail.com>
2024-07-12 12:12:39 -0300

    ex017

--- Aula14ex/ex17/script.js
+++ Aula14ex/ex17/script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+function tabuada(){
+    var num = window.document.getElementById('txtn')
+    var tab = window.document.getElementById ('seltab')
+
+    if(num.value.length == 0){
+        window.alert('Por favor, digite um número!')
+
+    }else{
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while(c<=10){
+            var item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n * c}`
+            tab.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+        
+    }
+}
