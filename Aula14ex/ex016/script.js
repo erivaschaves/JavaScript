92e59974e0d1903bb7f2d5fdac068fb649c912b6
ErivaldoChaves <erivas_12@hotmail.com>
2024-07-12 11:16:45 -0300

    ex016

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,27 +1,45 @@
-/*function contar(){
+function contar(){
     var inicio = window.document.getElementById('txtini')
-    var fim = window.document.querySelector('div#txtfim')
-    fim = Number(fim.value)
+    var fim = window.document.getElementById('txtfim')
     var passo = window.document.getElementById('txtpasso')
-    passo = Number(passo.value)
-    var res = Number(inicio.value)
-    inicio = 5
-    fim = 12
-    passo = 2
-    res = inicio
-    while(res<fim){
-    console.log(res)
-    res = res + passo
-}
-    
-    
-}*/ 
+    var res = window.document.getElementById('res')
+    if(inicio.value.length == 0|| fim.value.length == 0 || passo.value.length==0){
+        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossivel contar'
+    }else{
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if(p<=0){
+            window.alert('Passo Inválido! Considerando Passo 1')
+            p=1
+        }
+        // Contagem Crescente
+        if(i < f){
+            for (var c = i; c<=f; c+=p){
+                res.innerHTML += ` ${c} \u{1F449} ` 
+            }
+        // Contagem Decrescente   
+        } else{
+            for(var c = i ; c>=f; c-=p){
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += ` \u{1F3C1}`
 
-inicio = 5
+    }
+    
+    
+}
+
+
+
+/*inicio = 5
 fim = 12
 passo = 2
 res = inicio
 while(res<fim){
     console.log(res)
     res = res + passo
-}
+}*/
