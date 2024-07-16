e364364953b2e0bf52fe0d1e07254e3de0e774c1
ErivaldoChaves <erivas_12@hotmail.com>
2024-07-16 17:05:46 -0300

    ex018

--- Aula016ex/ex018/script.js
+++ Aula016ex/ex018/script.js
@@ -3,16 +3,12 @@
 //console.log ('Valor inválido ou já encontrado na lista.')
 var vetor = []
 var res = window.document.getElementById('res')
-var mai = window.document.getElementById('maior')
-var men = window.document.getElementById('menor')
-var som = window.document.getElementById('soma')
-var med = window.document.getElementById('media')
 function adicionar(n){
     var num = window.document.getElementById('txtn')
     var val = window.document.getElementById('selval')
     var n = Number(num.value)
     existe = false
-    if (n < 0 || n > 100 || n == 0){
+    if (Number(n) < 1 || Number(n) > 100){
         window.alert('Valor inválido ou já encontrado na lista.') 
     }else{
         for (c=0; c<vetor.length; c++){
@@ -26,34 +22,39 @@ function adicionar(n){
             item.text = `O Valor ${n} foi adicionado.`
             val.value = `val ${n}`
             val.appendChild(item)
-            vetor.push(Number(n))            
+            vetor.push(Number(n))
+            res.innerHTML = ''            
         }
     }
-    
+    num.value = ''
+    num.focus()
 }
 
 
 function finalizar(){
-res.innerHTML= `Ao todo temos ${vetor.length} números cadastrados.`
-maior = 0
-soma = 0
+    if(vetor.length == 0){
+        window.alert('Adicione Valores antes de Finalizar!')
 
-for (c = 0; c < vetor.length; c++){
-    if(maior < vetor[c]){
-        maior = vetor[c]
-    }
-    soma+= vetor[c]
-}
-menor = maior
-for (c = 0; c<vetor.length; c++){
-    if(menor > vetor[c]){
-        menor = vetor[c]
-    }
-}
-mai.innerHTML = `O maior valor informado foi ${maior}.` + '<br>'
-men.innerHTML = `O menor valor informado foi ${menor}.` + '<br>'
-som.innerHTML = `Somando todos os valores, temos ${soma}.`+ '<br>'
-med.innerHTML = `A média dos valores digitados é ${media = soma / vetor.length}.`
+    }else{       
+        maior = vetor[0]
+        soma = 0
+        menor = vetor[0]
+        for (c = 0; c < vetor.length; c++){
+            soma+= vetor[c]
+            if(maior < vetor[c]){
+               maior = vetor[c]
+            }
+            if(menor > vetor[c]){
+                menor = vetor[c]
+            }    
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${vetor.length} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media = soma / vetor.length}.</p>`
+        }
 }
 
     
