90da3de3985db86ff170ef2e5d063b5cebd0ec6a
ErivaldoChaves <erivas_12@hotmail.com>
2024-09-21 10:37:38 -0300

    Otimizando código

---
+++ Modelos/003 - Peso Ideal/script.js
@@ -0,0 +1,20 @@
+//Definindo função calcular
+function calcular(){
+    //Pegado valores do input pelo id
+    var talt = window.document.getElementById('alt');
+    //Convertendo valores para tipo number
+    var alt = Number(talt.value)
+    var res = window.document.getElementById('res');
+    // Validando a opção marcada e exbindo com 2 casas decimais com toFixed
+    if( window.document.getElementById('mas').checked) {
+        res.innerHTML = `Seu peso ideal é: ${((72.7 * alt) - 58).toFixed(2)} KG meu jovem.`;
+    }else{
+        res.innerHTML = `Seu peso ideal é: ${((62.1 * alt) - 44.7).toFixed(2)} KG minha jovem.`; 
+    }
+}
+
+function limpar(){
+    var inputVal = window.document.querySelector('input[type=Number]');
+    inputVal.value = " ";
+    res.innerHTML = "Resultado";
+}
